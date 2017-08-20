857b8ca05b5b9542fed922bcabac70ddaff82fbf
VLER <ycysvler@hotmail.com>
2017-08-20 15:29:56 +0800

    * 修改mysql服务器地址

--- mysql/mentmenu.js
+++ mysql/mentmenu.js
@@ -8,7 +8,7 @@ module.exports = class Mentmenu {
 
     /* 获取角色列表 */
     list(userid,callback) {
-        var sql = 'select mrr.rid ,re.rname ,re.url,re.parentid from map_role_resoure as mrr,map_role_user as mru,resource as re where mrr.rid=re.rid and mru.roleid = mrr.roleid and mru.userid=?';
+        var sql = 'select mrr.rid ,re.rname ,re.url,re.parentid from map_role_resoure as mrr,map_role_user as mru,resource as re where mrr.rid=re.rid and mru.roleid = mrr.roleid and mru.userid=? order by re.order';
         // 用pool查数据库
         pool.query(sql, [userid], function (error, rows, fields) {
             if (error) {
